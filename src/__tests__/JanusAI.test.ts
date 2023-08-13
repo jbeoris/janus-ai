fa14c16e7ccf378bbff9c35018b3bed0a010db4a
Jack Beoris <john.beoris@utexas.edu>
2023-08-12 22:39:42 -0400

    test

--- src/__tests__/JanusAI.test.ts
+++ src/__tests__/JanusAI.test.ts
@@ -1,4 +1,4 @@
-import JanusAI from '../index';
+import JanusAI, { RegisterChatOptions } from '../index';
 import { RedisClientType, createClient } from 'redis';
 
 const redis: RedisClientType = createClient()
@@ -34,3 +34,25 @@ test('Get Override Model RPM', () => {
   expect(makeDefaultJanus().getRequestLimit('gpt-3.5-turbo')?.count).toBe(3500);
   expect(makeOverrideJanus().getRequestLimit('gpt-3.5-turbo', 'one')?.count).toBe(200);
 });
+
+test('test rate limit', async () => {
+  const janus = makeDefaultJanus()
+
+  const chatInput: RegisterChatOptions = { 
+    model: 'gpt-3.5-turbo', 
+    data: [
+      { 
+        role: 'system',
+        content: 'you are an AI'
+      },
+      { 
+        role: 'user',
+        content: 'Hello'
+      }
+    ] 
+  }
+
+  const results = await janus.registerChatInput(chatInput)
+
+  console.log(results)
+})
